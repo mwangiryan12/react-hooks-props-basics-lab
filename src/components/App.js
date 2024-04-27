11ab5ee79aa5d9bc00d8647d2683b5c5d90c0d86
mwangiryan12 <mwangiryan12@gmail.com>
2024-04-27 13:42:10 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
@@ -5,14 +6,13 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio= {user.bio} links= {user.links} />
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,10 @@
+
+function Links (mwangi){
+        return(
+            <>
+            <h3>Links</h3>
+            <a href={mwangi.github}>{mwangi.github}</a>
+            <a href={mwangi.linkedin}>{mwangi.linkedin}</a>
+            </>
+        )
+    }export default Links
